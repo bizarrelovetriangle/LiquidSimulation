63639139bc984ae8fdc03fd1d0c5b03161df2f80
Томсон Ради <degemot@gmail.com>
2023-01-05 20:44:52 +0400

    Update c++ versions

--- LiquidSimuldation/LiquidSimuldation.cpp
+++ LiquidSimuldation/LiquidSimuldation.cpp
@@ -1,16 +1,16 @@
 #include "Scene.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <Windows.h>
-
-    __declspec(dllexport) DWORD NvOptimusEnablement = 1;
-    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-
-#ifdef __cplusplus
-}
-#endif
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
+//#include <Windows.h>
+//
+//    __declspec(dllexport) DWORD NvOptimusEnablement = 1;
+//    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+//
+//#ifdef __cplusplus
+//}
+//#endif
 
 int main()
 {
